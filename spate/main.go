289847b8d11b27619b2ffd4b3dafff86b0b0429d
Johannes Wünsche <johannes.wuensche97@gmail.com>
2021-02-01 18:12:54 +0100

    Move FastRand to Struct

--- spate/main.go
+++ spate/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"context"
+	"encoding/binary"
 	"fmt"
 	"math/rand"
-	"context"
 	"net"
 	"time"
 
@@ -15,16 +16,28 @@ import (
 var nillyboii error = nil
 
 // Xorshift
-type FastRand uint64
-func NewFastRand() FastRand {
-	return FastRand(rand.Uint64())
+type FastRand struct {
+	value uint64
+	size  uint64
+	buf   []byte
 }
 
-func (r FastRand) Get() uint64 {
-	r ^= r << 13
-	r ^= r >> 7
-	r ^= r << 17
-	return uint64(r)
+func NewFastRand(size uint64) FastRand {
+	return FastRand{value: rand.Uint64(), buf: make([]byte, size), size: size}
+}
+
+func (r FastRand) Get() *[]byte {
+	for k := uint64(0); k < r.size/uint64(8); k++ {
+		r.value ^= r.value << 13
+		r.value ^= r.value >> 7
+		r.value ^= r.value << 17
+		binary.BigEndian.PutUint64(r.buf[k*8:], r.value)
+	}
+	r.value ^= r.value << 13
+	r.value ^= r.value >> 7
+	r.value ^= r.value << 17
+	binary.BigEndian.PutUint64(r.buf[r.size-8:], r.value)
+	return &r.buf
 }
 
 func spawnServer(port uint16) error {
@@ -33,9 +46,9 @@ func spawnServer(port uint16) error {
 	conn, err := appnet.ListenPort(port)
 	if err != nil {
 		return err
-        }
+	}
 
-        recv := make([]byte, 2500)
+	recv := make([]byte, 2500)
 	start := time.Now()
 	duration, err := time.ParseDuration(runtime)
 	if err != nil {
@@ -43,61 +56,64 @@ func spawnServer(port uint16) error {
 	}
 	end := start.Add(duration)
 	bytes_recvd := 0
-        //... handling logic, fetch individual packet
-        for {
-                // Handle client requests
-                resp_length, _, err := conn.ReadFrom(recv)
-                if err != nil {
-                        continue
-                }
+	//... handling logic, fetch individual packet
+	for {
+		// Handle client requests
+		resp_length, _, err := conn.ReadFrom(recv)
+		if err != nil {
+			continue
+		}
 		// invalid length
-                if resp_length < 1 {
-                        continue
-                }
+		if resp_length < 1 {
+			continue
+		}
 
 		bytes_recvd += resp_length
 
 		if time.Until(end) <= 0 {
 			break
 		}
-        }
+	}
 	elapsed := time.Since(start)
 	throughput := float64(bytes_recvd) / elapsed.Seconds() / 1024.0 / 1024.0 * 8.0
 
-	fmt.Printf("Throughput: %d Mibit/s\n", throughput)
+	fmt.Printf("Throughput: %f Mibit/s\n", throughput)
 
 	return nillyboii
 }
 
 func connectTest(serverAddrStr string) {
 	serverCCAddr, err := appnet.ResolveUDPAddr(serverAddrStr)
-        if err != nil {
-                fmt.Printf("%s%s", "Resolution of UDP address failed, to: ", serverAddrStr)
-        }
+	if err != nil {
+		fmt.Printf("%s%s", "Resolution of UDP address failed, to: ", serverAddrStr)
+	}
 
-        var metric int
-        path, err := appnet.ChoosePathByMetric(metric, serverCCAddr.IA)
+	var metric int
+	path, err := appnet.ChoosePathByMetric(metric, serverCCAddr.IA)
 	if err != nil {
 		fmt.Println("Cannot choose selected path")
 	}
-        if path != nil {
+	if path != nil {
 		// Set selected singular path
-                appnet.SetPath(serverCCAddr, path)
-        }
+		appnet.SetPath(serverCCAddr, path)
+	}
 
 	CCConn, err := appnet.DialAddr(serverCCAddr)
-                // get the port used by clientCC after it bound to the dispatcher (because it might be 0)
-        clientCCAddr := CCConn.LocalAddr().(*net.UDPAddr)
-        // Address of client data channel (DC)
-        clientDCAddr := &net.UDPAddr{IP: clientCCAddr.IP, Port: clientCCAddr.Port + 1}
-        // Address of server data channel (DC)
-        serverDCAddr := serverCCAddr.Copy()
-        serverDCAddr.Host.Port = serverCCAddr.Host.Port + 1
+	// get the port used by clientCC after it bound to the dispatcher (because it might be 0)
+	clientCCAddr := CCConn.LocalAddr().(*net.UDPAddr)
+	// Address of client data channel (DC)
+	clientDCAddr := &net.UDPAddr{IP: clientCCAddr.IP, Port: clientCCAddr.Port + 1}
+	// Address of server data channel (DC)
+	serverDCAddr := serverCCAddr.Copy()
+	serverDCAddr.Host.Port = serverCCAddr.Host.Port + 1
 
-        // Create specific data connection to server
-	_, err = appnet.DefNetwork().Dial(context.TODO(), "udp", clientDCAddr, serverDCAddr, addr.SvcNone)
-
+	// Create specific data connection to server
+	DCConn, err := appnet.DefNetwork().Dial(context.TODO(), "udp", clientDCAddr, serverDCAddr, addr.SvcNone)
 	//TODO: Send Data on Data Connection And init measurement
+	rand := NewFastRand(8)
+	for {
+		DCConn.Write(*rand.Get())
+	}
 }
 
 func main() {
@@ -105,4 +121,3 @@ func main() {
 
 	spawnServer(1337)
 }
-
--- spate/main_test.go
+++ spate/main_test.go
@@ -6,11 +6,9 @@ import (
 
 // achieves ~30 GB/s on Ryzen 5 3600
 func BenchmarkXorshift(b *testing.B) {
-	var frand = NewFastRand()
 	for n := 0; n < b.N; n++ {
+		var frand = NewFastRand(134217728)
 		// get 1 GibiByte of random data
-		for c := 0; c < 134217728; c++ {
-			frand.Get()
-		}
+		frand.Get()
 	}
 }
