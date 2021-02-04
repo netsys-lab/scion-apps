5a2630d387edeadb99478bd2969c95c55cb5b596
Fin Christensen <christensen.fin@gmail.com>
2021-02-04 15:12:44 +0100

    Add logging for client

--- spate/client.go
+++ spate/client.go
@@ -30,6 +30,7 @@ func (s SpateClientSpawner) PacketSize(packet_size int) SpateClientSpawner {
 }
 
 func (s SpateClientSpawner) Spawn() error {
+	Info("Resolving address %s...", s.server_address)
 	serverAddr, err := appnet.ResolveUDPAddr(s.server_address)
 	if err != nil {
 		Error("Resolution of UDP address (%s) failed: %v", s.server_address, err)
@@ -45,19 +46,22 @@ func (s SpateClientSpawner) Spawn() error {
 	// )
 	var metric = 0 // Default
 
+	Info("Selecting paths to remote...")
 	path, err := appnet.ChoosePathByMetric(metric, serverAddr.IA)
 	if err != nil {
 		Warn("Could not choose path with metric: %v", err)
 	}
 	if path != nil {
 		// Set selected singular path
-		Info("Selecting path: %v", path)
+		Info("Choosing the following path: %v", path)
 		appnet.SetPath(serverAddr, path)
 	}
 
+	Info("Establishing connection with server...")
 	DCConn, err := appnet.DialAddr(serverAddr)
 	rand := NewFastRand(uint64(s.packet_size))
 	start := time.Now()
+	Info("Starting sending data for measurements")
 	for {
 		_, err = DCConn.Write(*rand.Get())
 		if err != nil {
@@ -66,7 +70,7 @@ func (s SpateClientSpawner) Spawn() error {
 		}
 	}
 	elapsed := time.Since(start)
-	Info("Sending took %s", elapsed)
+	Info("Finished sending packets, operation took %s", elapsed)
 
 	return nil
 }
