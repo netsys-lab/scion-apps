9b60be1125cf2835dfad869b3555f707503dfc7e
Fin Christensen <christensen.fin@gmail.com>
2021-02-13 17:24:37 +0100

    Add parse function for Mib, MiB, Mb, MB, etc.

---
+++ spate/bits.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/alecthomas/units"
+)
+
+var (
+	bytesUnitMap = units.MakeUnitMap("iB", "B", 1024)
+	metricBytesUnitMap = units.MakeUnitMap("B", "B", 1000)
+	bitsUnitMap = units.MakeUnitMap("ib", "b", 1024)
+	metricBitsUnitMap = units.MakeUnitMap("b", "b", 1000)
+)
+
+func ParseBits(s string) (int64, error) {
+	n, err := units.ParseUnit(s, bytesUnitMap)
+	if err == nil {
+		n *= 8
+	} else {
+		n, err = units.ParseUnit(s, metricBytesUnitMap)
+
+		if err == nil {
+			n *= 8
+		} else {
+			n, err = units.ParseUnit(s, bitsUnitMap)
+
+			if err != nil {
+				n, err = units.ParseUnit(s, metricBitsUnitMap)
+			}
+		}
+	}
+
+	return n, err
+}
